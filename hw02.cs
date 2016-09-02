19da559fdbfd66485ae825cc73760061eacd2acf
Paul Ramnora <pramnora@yahoo.com>
2016-09-02 01:43:02 +0100

    Create hw02.cs
    This 2nd version of 'Hello, world!' program will accept two arguments after running it from an MS DOS prompt box: C:\>hw01 one two

---
+++ hw02.cs
@@ -0,0 +1,29 @@
+/* CREATED: 2nd Sep 2016 01:33 AM GMT
+   UPDATED: 2nd Sep 2016 01:33 AM GMT
+
+   NOTE: This is my 2nd 'Hello, world!' C# program...
+
+         The program uses 2 command line arguments each called: arg[0],arg[1]; 
+         both arguments will need to be entered from the user keyboard;
+         the program is first run inside of a MS DOS prompt box by typing:
+         hw02 "one" "two"
+         ...the output displays as...
+         Hello, world! one two
+
+         (NOTE: In order to run the program within DOS you do not need to type the full filename + . + filenameExtension: hw02.exe.)
+*/
+
+using System;
+
+namespace hw02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world! "  
+                + args[0] + " " + args[1]);
+            Console.Read();
+        }
+    }
+}
